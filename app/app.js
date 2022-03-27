40cb61fcfca1aacebe6201c41a8c44a314040c19
bluesun <amis5818@hanmail.net>
2022-03-27 22:30:19 +0900

    프런트 단 자바스크립트 파일 만들기

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@
 // 모듈
 import express from "express";
 const app = express();
+import path from "path";
+const __dirname = path.resolve();
 
 // 라우팅
 import home from "./src/routes/home/index.js";
@@ -11,6 +13,7 @@ import home from "./src/routes/home/index.js";
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //use 미들 웨어를 등록해주는 메서드
 
