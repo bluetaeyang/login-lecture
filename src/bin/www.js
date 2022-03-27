39dd2c9e8ee4f1c0bb0efecf08f834fd50554f0c
bluesun <amis5818@hanmail.net>
2022-03-27 21:31:31 +0900

    폴더구조 정리

--- src/bin/www.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import { app } from "../app.js";
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log("서버가 시작되었습니다");
-});
--- src/routes/home/home.ctrl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const home = (req, res) => res.render("home/index");
-const login = (req, res) => res.render("home/login");
-
-export { home, login };
